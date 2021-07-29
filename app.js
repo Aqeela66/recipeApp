59b2442605703019796e9e7c17f413926b08d681
Aqeela <83452367+Aqeela66@users.noreply.github.com>
2021-07-30 01:24:11 +0500

    implement Recipe App using HTML , CSS and JS through API

--- app.js
+++ app.js
@@ -10,27 +10,27 @@ fetch(`${base}/complexSearch?apiKey=${key}&query=${query}`)
     }).then(function(recipesArray) {
         console.log('All recipes', recipesArray);
 
-        let image1 = recipesArray.results[1].image;
-        let image2 = recipesArray.results[2].image;
-        let image3 = recipesArray.results[3].image;
-        let image4 = recipesArray.results[4].image;
-        let image5 = recipesArray.results[5].image;
-        let image6 = recipesArray.results[6].image;
-        document.getElementsByClassName("recipe-card")[0].innerHTML = recipesArray.results[1].title;
+        let image1 = recipesArray.results[9].image;
+        let image2 = recipesArray.results[8].image;
+        let image3 = recipesArray.results[7].image;
+        let image4 = recipesArray.results[6].image;
+        let image5 = recipesArray.results[2].image;
+        let image6 = recipesArray.results[4].image;
+        document.getElementsByClassName("recipe-card")[0].innerHTML = recipesArray.results[9].title;
         document.getElementsByClassName("recipe-card")[0].style.backgroundImage = `url(${image1})`;
 
-        document.getElementsByClassName("recipe-card")[1].innerHTML = recipesArray.results[2].title;
+        document.getElementsByClassName("recipe-card")[1].innerHTML = recipesArray.results[8].title;
         document.getElementsByClassName("recipe-card")[1].style.backgroundImage = `url(${image2})`;
 
-        document.getElementsByClassName("recipe-card")[2].innerHTML = recipesArray.results[3].title;
+        document.getElementsByClassName("recipe-card")[2].innerHTML = recipesArray.results[7].title;
         document.getElementsByClassName("recipe-card")[2].style.backgroundImage = `url(${image3})`;
 
-        document.getElementsByClassName("recipe-card")[3].innerHTML = recipesArray.results[4].title;
+        document.getElementsByClassName("recipe-card")[3].innerHTML = recipesArray.results[6].title;
         document.getElementsByClassName("recipe-card")[3].style.backgroundImage = `url(${image4})`;
 
-        document.getElementsByClassName("recipe-card")[4].innerHTML = recipesArray.results[5].title;
+        document.getElementsByClassName("recipe-card")[4].innerHTML = recipesArray.results[2].title;
         document.getElementsByClassName("recipe-card")[4].style.backgroundImage = `url(${image5})`;
 
-        document.getElementsByClassName("recipe-card")[5].innerHTML = recipesArray.results[6].title;
+        document.getElementsByClassName("recipe-card")[5].innerHTML = recipesArray.results[4].title;
         document.getElementsByClassName("recipe-card")[5].style.backgroundImage = `url(${image6})`;
     });
